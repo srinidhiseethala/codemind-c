7321086847e3adda629f5c7aabc70c3c39f34648
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2023-01-04 19:59:28 +0530

    #include<stdio.h> int main() {     int n,x;     scanf("%d",&n);     while(n--)     {         scanf("%d",&x);         if(x>30)         {             printf("YES ");         }         else         {             printf("NO ");         }     } }

---
+++ Subscription.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,x;
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%d",&x);
+        if(x>30)
+        {
+            printf("YES
+");
+        }
+        else
+        {
+            printf("NO
+");
+        }
+    }
+}
