20477a473d7f81f8ade61dcdbd539eac0cb37151
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2023-01-15 17:14:48 +0530

    #include<stdio.h> int main() {     int a,b,c,d;     scanf("%d%d%d%d",&a,&b,&c,&d);     if(a,b>10)     {         if(c,d>=10)         {             printf("YES");         }         else         {             printf("NO");         }     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    if(a,b>10)
+    {
+        if(c,d>=10)
+        {
+            printf("YES");
+        }
+        else
+        {
+            printf("NO");
+        }
+    }
+    else
+    {
+        printf("NO");
+    }
+}
