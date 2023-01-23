7418c6c98026070423e8e337409c37ea18d9297e
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2023-01-23 19:43:38 +0530

    #include<stdio.h> int main() {     int n,m,h,a;     scanf("%d%d",&n,&m);     while(n!=m)     {         if(n>m)         {             n=n-m;         }         else{             m=m-n;         }     }     printf("%d",n); }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,h,a;
+    scanf("%d%d",&n,&m);
+    while(n!=m)
+    {
+        if(n>m)
+        {
+            n=n-m;
+        }
+        else{
+            m=m-n;
+        }
+    }
+    printf("%d",n);
+}
