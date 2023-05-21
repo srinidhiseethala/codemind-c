2748ac14acd96a094272739ba1750fd58ccb8a5c
srinidhiseethala <119839619+srinidhiseethala@users.noreply.github.com>
2023-05-21 08:53:10 +0530

    #include <stdio.h>
    int addDigits(int num);
    
    int main() {
        int num;
        scanf("%d", &num);
        if (num < 0) {
            printf("Invalid input. Please enter a non-negative integer.
    ");
        } else {
            int result = addDigits(num);
            printf("%d
    ", result);
        }
        
        return 0;
    }
    
    int addDigits(int num) {
        while (num >= 10) {
            int sum = 0;
            while (num > 0) {
                sum += num % 10;
                num /= 10;
            }
            num = sum;
        }
        
     return num;
    }

---
+++ Add_Digits.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int addDigits(int num);
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    if (num < 0) {
+        printf("Invalid input. Please enter a non-negative integer.
+");
+    } else {
+        int result = addDigits(num);
+        printf("%d
+", result);
+    }
+    
+    return 0;
+}
+
+int addDigits(int num) {
+    while (num >= 10) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        num = sum;
+    }
+    
+ return num;
+}
